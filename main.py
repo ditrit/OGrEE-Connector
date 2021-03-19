7cc2e891bfa02a1587f8f2fc7ce50d9ad2ad5c8f
Ziad K <zkhalafeu01@tutanota.com>
2021-03-19 16:09:35 +0100

    Added Auth Token segment and testing segment to understand how to make an API request using the libs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import requests, json
 
+#Auth Token
+
+
 # Obtain the big list of racks
 # r = requests.get("https://dcim.chibois.net/api/dcim/racks/")
 # parsed = json.loads(r)
@@ -22,4 +25,10 @@ import requests, json
 
 
 
-### Test with Current API @ https://api.chibois.net
+### Test with Current API @ https://api.chibois.net/api/user/
+
+r = requests.get("https://api.chibois.net/api/user/tenants")
+print(r.json())
+
+'''for idx in parsed:
+    print(idx)'''
