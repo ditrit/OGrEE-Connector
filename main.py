bbe4ddaf02ce803162f2e3fa587f871f52401833
Ziad K <zkhalafeu01@tutanota.com>
2021-03-23 18:06:00 +0100

    The script is now able to pull tenant data from Netbox, transform it and POST it to the DB via my GO API

--- main.py
+++ main.py
@@ -12,27 +12,62 @@ dhead = {'Authorization': 'Token {}'.format(dcim_token)}
 # URLs
 # LOCAL: http://localhost:8000/api/user/tenants
 # DCIM: https://api.chibois.net/api/dcim/racks/
-
-# Obtain the big list of racks
-# r = requests.get("https://dcim.chibois.net/api/dcim/racks/")
-# parsed = json.loads(r)
-# ITERATE THROUGH EACH ELEMENT AND SEND AN API REQUEST
+# NETBX: https://dcim.chibois.net/api/dcim/racks
 
 
-
-# Obtain the big list of sites
-# r = requests.get("https://dcim.chibois.net/api/dcim/sites/")
-# parsed = json.loads(r)
-# ITERATE THROUGH EACH ELEMENT AND SEND AN API REQUEST
-
-
-
-# Obtain the big list of tenants 
-# print(dhead)
+# Obtain the big list of Sites
 r = requests.get("https://dcim.chibois.net/api/dcim/sites/", headers=dhead)
-print(r.json())
-# parsed = json.loads(r)
+# print(r.json())
 # ITERATE THROUGH EACH ELEMENT AND SEND AN API REQUEST
+#for site in r.json()['results']:
+#    print("Here's a site!")
+
+
+
+# Obtain the big list of Devices 
+
+r = requests.get("https://dcim.chibois.net/api/dcim/devices/", headers=dhead)
+# print(r.json()['results'])
+
+for d in r.json()['results']:
+    if d['tenant'] != None:
+        print("U NOT LIKE ME")
+
+
+# Obtain the big list of Racks 
+
+r = requests.get("https://dcim.chibois.net/api/dcim/racks/", headers=dhead)
+# print(r.json()['results'])
+
+for d in r.json()['results']:
+    if d['tenant'] != None:
+        print("U NOT LIKE ME")
+
+
+
+
+
+
+# Obtain the big list of Tenants
+r = requests.get("https://dcim.chibois.net/api/tenancy/tenants/", headers=dhead)
+
+for tn in r.json()['results']:
+    npq = {
+  "name": tn['name'],
+  "id": int(tn['id']),
+  "parentId": None,
+  "category": "tenant",
+  "description": [tn['description']],
+  "domain": "Connector Domain",
+  "attributes": {
+    "color": "Connector Color",
+    "mainContact": None,
+    "mainPhone": None,
+    "mainEmail": None
+  }
+}
+    pr = requests.post("https://api.chibois.net/api/user/tenants", headers=head, data=json.dumps(npq) )
+    print(pr.json())
 
 
 
@@ -40,34 +75,3 @@ print(r.json())
 ### To check that a request is successful, 
 # use r.raise_for_status() or check r.status_code is what you expect
 
-# r = requests.get("https://api.chibois.net/api/user/tenants", headers=head)
-# print(r.json())
-
-# parsed = json.loads(r.json())
-
-""" for k in r.json():
-    print(r.json()[k]) """
-
-### print(r.json()['data'][1])
-
-### ITERATE THROUGH EACH OBJECT IN JSON RESPONSE
-'''for obj in r.json()['data']:
-    print(obj)'''
-
-
-""" r = requests.put("http://localhost:8000/api/user/tenants", headers=head,
-data=r.json()['data'][0]) """
-
-# Q = r.json()['data'][1]
-# Q['id'] = None
-# Q['attributes']['id'] = None
-# Q['attributes']['color'] = "NIL"
-# Q['category'] = "THE HOTTEST DNB"
-# Q['name'] = "RONI SIZE"
-
-
-# print(json.dumps(Q))
-
-
-# fg = requests.post('http://localhost:8000/api/user/tenants',data=json.dumps(Q), headers=head)
-# print(fg)
