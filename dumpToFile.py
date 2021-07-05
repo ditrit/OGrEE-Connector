2a92646f60be82393ba7d78508f320fd6b33e710
Ziad K <zkhalafeu01@tutanota.com>
2021-07-05 14:28:20 +0200

    Made dumpToFile able to produce a list for single object list

--- dumpToFile.py
+++ dumpToFile.py
@@ -41,12 +41,18 @@ parser = argparse.ArgumentParser(description='Import from Netbox to file .')
 parser.add_argument('--objects', choices=["tenant","site", "bldg", "room", 
                     "rack", "device", "subdevice", "subdevice1"],
                     help="""Option to select which objects to add.
-                    Note they are inclusive (ie specifying subdevices
-                    will import everything until subdevices). Default: Rack""")
+                    Note they are inclusive . Default: Rack""")
+parser.add_argument("--inclusive",
+help="""Specifies whether to include eveything until object 
+(ie if subdevices and inclusive were specified 
+then everything until subdevices will be imported). 
+                    If not enabled then false""", action="store_true")
 parser.add_argument('--dir',
                     help="""Location to store JSON results.
                     All results will be stored in a text file named 
                     \'{entity}List.json\'""")
+parser.add_argument('--NBURL', 
+help="""Specify URL of Netbox. Default: https://dcim.chibois.net/api""")
 
 #FUNCTION DEFINTIONS
 def getObjList(url):
@@ -66,6 +72,7 @@ def dumpListToFile(objList, dir, entType):
 
 
 #START
+x = 0
 args = vars(parser.parse_args())
 if ('dir' not in args or args['dir'] == None):
     print('Directory not specified... using current dir')
@@ -74,27 +81,37 @@ else:
     dir = args['dir']
 
 
+
 if args['objects'] != None:
     depth = objToHierarchyDict[args['objects']].value
+    if ('inclusive' in args): 
+        if args['inclusive'] != True:
+            x = objToHierarchyDict[args['objects']].value
 else:
     print('Objects not specified... using Rack as dump limit')
     depth = Hierarchy(4).value #RACK
 
+if ('NBURL' not in args or args['NBURL'] == None):
+    print('Netbox URL not specified... using default URL')
+    NBURL = "https://dcim.chibois.net/api"
+else:
+    NBURL = args['NBURL']
 
-x = 0
+
+
 while x < (depth+1):
     if x == 0: # TENANT
-        url = "https://dcim.chibois.net/api/tenancy/tenants/"
+        url = NBURL+"/tenancy/tenants/"
     if x == 1: # SITE
-        url = "https://dcim.chibois.net/api/dcim/sites/"
+        url = NBURL+"/dcim/sites/"
     if x == 2: # BLDG --> ROOM
         print("No such thing as buildings, skipping")
         x+=1
-        url = "https://dcim.chibois.net/api/dcim/rack-groups/"
+        url = NBURL+"/dcim/rack-groups/"
     if x == 4: # RACK
-        url = "https://dcim.chibois.net/api/dcim/racks/"
+        url = NBURL+"/dcim/racks/"
     if x == 5: # DEVICE
-        url = "https://dcim.chibois.net/api/dcim/devices/"
+        url = NBURL+"/dcim/devices/"
     objList =  getObjList(url)
     dumpListToFile(objList, dir, Hierarchy(x))
     x += 1
